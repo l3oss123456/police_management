70e54842b2a2848b85cd685044f204106a0a32f7
l3oss123456 <oncelemon02@gmail.com>
2020-02-23 23:51:35 +0700

    can show data / search and delete data

---
+++ src/containers/adminManage/components/editAdmin/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FormAdmin from "../formAdmin/index";
+// import axios from "../../../../core/libs/axios/axios";
+
+const editAdmin = props => {
+  // const resp = await axios("GET", `/officers`);
+  return (
+    <div>
+      <FormAdmin />
+    </div>
+  );
+};
+export default editAdmin;
