2fe342faacc2dfa5abac56305d0be2f61f195af1
l3oss123456 <oncelemon02@gmail.com>
2020-03-10 14:50:33 +0700

    [feature/login]: new decorate login form

--- src/containers/login/components/Styled/index.js
+++ src/containers/login/components/Styled/index.js
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 import { Card, Button } from "antd";
+import Theme from "../../../../core/theme/index";
 
 export const StyledBackgroundLayout = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background: ${Theme.colors.midnightEx};
 `;
 
 export const StyledImgLayout = styled.img`
-  height: 100vh;
-  width: 100vw;
+  height: 90px;
+  width: 90px;
 
   background-position: center;
   background-repeat: no-repeat;
@@ -17,7 +21,7 @@ export const StyledImgLayout = styled.img`
 `;
 
 export const StyledCard = styled(Card)`
-  height: 380px;
+  height: 420px;
   width: 380px;
 
   border-radius: 20px;
@@ -29,10 +33,12 @@ export const StyledCard = styled(Card)`
 `;
 
 export const StyledHeader = styled.div`
-  font-size: 36px;
+  font-family: ${Theme.fontFamily.primary}
+  font-size: 18px;
   font-weight: bold;
-
-  width: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const StyledLogin = styled.div`
@@ -41,7 +47,7 @@ export const StyledLogin = styled.div`
 
 export const StyledWarning = styled.div`
   color: red;
-  font-size: 13px;
+  font-size: 10px;
 `;
 export const StyledButton = styled(Button)`
   .ant-btn {
